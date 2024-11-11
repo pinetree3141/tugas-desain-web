4a2ed4614a973d492ecc327b2058a24dfe16c5b1
Pine Firebloom <106411331+pinetree3141@users.noreply.github.com>
2024-11-11 07:00:45 +0700

    Updated

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 const galleryContainer = document.querySelector("gallery-container")
 imageContainer = galleryContainer.querySelectorAll("image-container")
+image = imageContainer.querySelector("image-container img")
+
